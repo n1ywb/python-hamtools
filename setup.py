76ab3fc1bdc36f4bfb8052de4d5ae57ca5f397b4
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2016-10-04 09:22:55 -0400

    0.2

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ from distutils.core import setup
 
 setup(
     name='hamtools',
-    version='0.1',
+    version='0.2',
     description='N1YWB Python Ham Radio Tools',
     author='Jeff Laughlin',
-    author_email='n1ywb@arrl.net',
+    author_email='jeff@jefflaughlinconsulting.com',
     url='https://github.com/n1ywb/python-hamtools',
+    download_url='https://github.com/n1ywb/python-hamtools/archive/0.2.zip',
     packages = ['hamtools'],
     package_data={'hamtools': ['ctydat/cty.dat']},
     scripts = ['geolog', 'vk'],
     install_requires = ['geojson', 'requests', 'requests-cache'],
+    keywords = ['ham', 'amateur', 'qrz', 'adif', 'cabrillo', 'kml', 'geojson', 'ctydat'],
     long_description = (
 """Collection of amateur radio tools. Includes ability to read ADIF and
-Cabrillo log files, cty.dat files, georeference callsigns via QRZ.com and
-cty.dat, and output GIS data in GeoJSON and KML formats.""")
+Cabrillo log files, cty.dat files, georeference callsigns via QRZ.com,
+callook, and ctydat, and output GIS data in GeoJSON and KML formats.""")
 )
-
